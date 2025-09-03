3283940e47167202296baf079bd022492bf3b251
favibe <ibefavour85@gmail.com>
2025-09-03 22:30:32 +0100

    queryselector js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const calculator = document.querySelector(".container");
 const display = document.querySelector(".display");
 const buttons = document.querySelector(".buttons");
+
+let fisrtNumber = "";
+let secondNumber = "";
+let currentOperator = "";
+
