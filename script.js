7d2812e77f284222281512eb53c141b88b913707
favibe <ibefavour85@gmail.com>
2025-09-04 09:00:44 +0100

    operators function created

--- script.js
+++ script.js
@@ -1,8 +1,56 @@
-const calculator = document.querySelector(".container");
-const display = document.querySelector(".display");
-const buttons = document.querySelector(".buttons");
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    if (b === 0) return "Math Error"
+    return a / b;
+}
+
+function pow(a, b) {
+    return a ** b;
+}
+
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case  "-":
+            return sub(a, b);
+        case "*":
+            return mul(a, b);
+        case "/":
+            return div(a, b);
+        case "**":
+            return pow(a, b);
+        default:
+            return "";
+    }
+}
+
 
 let fisrtNumber = "";
 let secondNumber = "";
 let currentOperator = "";
+let shouldResetDisplay = "";
+
+const calculator = document.querySelector(".container");
+const display = document.querySelector(".display");
+const numberButton = document.querySelector(".number");
+const operatorButton = document.querySelector(".operator");
+const delButton = document.querySelector(".del");
+const clearButton = document.querySelector(".clear");
+const resultButton = document.querySelector(".result");
+
 
