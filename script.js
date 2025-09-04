17dc732423c68ae341ed272ad0ce7706f2a21d9b
favibe <ibefavour85@gmail.com>
2025-09-04 10:23:38 +0100

    evalute(), clear done

--- script.js
+++ script.js
@@ -28,11 +28,11 @@ function operate(operator, a, b) {
             return add(a, b);
         case  "-":
             return sub(a, b);
-        case "*":
+        case "x":
             return mul(a, b);
         case "/":
             return div(a, b);
-        case "**":
+        case "^":
             return pow(a, b);
         default:
             return "";
@@ -40,17 +40,77 @@ function operate(operator, a, b) {
 }
 
 
-let fisrtNumber = "";
+let firstNumber = "";
 let secondNumber = "";
 let currentOperator = "";
 let shouldResetDisplay = "";
 
 const calculator = document.querySelector(".container");
 const display = document.querySelector(".display");
-const numberButton = document.querySelector(".number");
-const operatorButton = document.querySelector(".operator");
+const numberButton = document.querySelectorAll(".number");
+const operatorButton = document.querySelectorAll(".operator");
 const delButton = document.querySelector(".del");
 const clearButton = document.querySelector(".clear");
 const resultButton = document.querySelector(".result");
 
+function appendToDisplay(content) {
+    //number limit on screen
+    if (display.textContent.length >= 12 && !shouldResetDisplay) return; 
+    // reset display
+    if (display.textContent === "0" || shouldResetDisplay) {
+        display.textContent = "";
+        shouldResetDisplay = false;
+    }
+    // prevent multiple decimals
+    if (content === "." && display.textContent.includes(".")) return;
 
+    //append display
+    display.textContent += content;
+}
+
+numberButton.forEach(button => {
+  button.addEventListener("click", () => appendToDisplay(button.textContent));
+});
+
+
+// operator function
+function setOperator(operator) {
+    if (currentOperator !== ""){
+         evaluate();
+        }
+    firstNumber = display.textContent;
+    currentOperator = operator;
+    shouldResetDisplay = true;
+}
+
+
+//operator button
+operatorButton.forEach(button => {
+    button.addEventListener("click", () => setOperator(button.textContent));
+});
+
+//result button
+
+function evaluate() {
+    if (currentOperator === "" || shouldResetDisplay) return;
+    secondNumber = display.textContent;
+    let  result = operate(currentOperator, firstNumber, secondNumber);
+    if (typeof result === "number") {
+        result = Math.round(result*1000) / 1000;
+    }
+
+    display.textContent = result;
+    firstNumber = result;
+    currentOperator = "";
+}
+resultButton.addEventListener("click", evaluate);
+
+//claer button
+clearButton.addEventListener("click", clear);
+function clear () {
+    display.textContent = "0";
+    firstNumber = "";
+    secondNumber = "";
+    currentOperator = "";
+    shouldResetDisplay = false;
+}
